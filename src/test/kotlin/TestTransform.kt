ea6e47907aab477f504505da547d787bc6356b40
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-08-27 00:47:56 +0200

    Add watermark filter to image transformation test
    Included a watermark filter to the image transformation settings. Adjusted resolution and reformatting code for readability. This enhances the testing of image processing functionalities, specifically for watermark application.

--- src/test/kotlin/TestTransform.kt
+++ src/test/kotlin/TestTransform.kt
@@ -1,16 +1,20 @@
+import com.sksamuel.scrimage.FontUtils
 import com.sksamuel.scrimage.ScaleMethod
 import com.sksamuel.scrimage.angles.Degrees
 import io.github.breninsul.simpleimageconvertor.dto.ImageFormat
 import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.OverlaySettings
 import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.Resolution
 import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.ScaleToSettings
+import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.TransformFunctionSettings
+import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.filter.WatermarkStampFilterSettings
 import io.github.breninsul.simpleimageconvertor.dto.writer.ConvertSettings
 import io.github.breninsul.simpleimageconvertor.service.consumer.DefaultImageConsumer
 import io.github.breninsul.simpleimageconvertor.service.processor.ImageProcessorService
 import org.junit.jupiter.api.Test
+import java.awt.Color
+import java.awt.Font
 import java.io.ByteArrayOutputStream
 import java.io.File
-import io.github.breninsul.simpleimageconvertor.dto.settings.transformation.TransformFunctionSettings
 
 class TestTransform {
     val processor = ImageProcessorService.Default
@@ -23,9 +27,14 @@ class TestTransform {
         val file = File(javaClass.classLoader.getResource("images/animated-webp-supported.webp").toURI())
         val outFile = File("testtransform/animated-scaled.${format.name.lowercase()}")
         outFile.createNewFile()
-        processor.process({ file.inputStream() }, { outFile.outputStream() },
+        processor.process(
+            { file.inputStream() }, { outFile.outputStream() },
             writerSettings = listOf(ConvertSettings(format = format)),
-            transformSettings = listOf(ScaleToSettings(Resolution(100, 100), ScaleMethod.FastScale), TransformFunctionSettings{img, st -> img.rotate(Degrees(90))}),
+            transformSettings = listOf(
+                ScaleToSettings(Resolution(1000, 1000), ScaleMethod.FastScale),
+                TransformFunctionSettings { img, st -> img.rotate(Degrees(90)) },
+                WatermarkStampFilterSettings("Test!!!", alpha = 0.9, colour = Color.RED, font = FontUtils.createFont(Font.SANS_SERIF, 60))
+            ),
             readerSettings = listOf(),
         )
         println("${outFile.absolutePath} took ${System.currentTimeMillis() - time}ms")
@@ -41,7 +50,7 @@ class TestTransform {
         processor.process(
             { file.inputStream() }, { secondAnimationOutputStream },
             writerSettings = listOf(ConvertSettings(format = format)),
-            transformSettings = listOf(ScaleToSettings(Resolution(100, 100), ScaleMethod.FastScale), TransformFunctionSettings{img, st -> img.rotate(Degrees(90))}),
+            transformSettings = listOf(ScaleToSettings(Resolution(100, 100), ScaleMethod.FastScale), TransformFunctionSettings { img, st -> img.rotate(Degrees(90)) }),
             readerSettings = listOf(),
         )
         val secondAnimationBytes = secondAnimationOutputStream.toByteArray()
@@ -49,7 +58,8 @@ class TestTransform {
         processor.process(
             { file.inputStream() }, { outFile.outputStream() },
             writerSettings = listOf(ConvertSettings(format = format)),
-           transformSettings =  listOf(OverlaySettings(0, 0, secondAnimation)),
+            transformSettings = listOf(OverlaySettings(0, 0, secondAnimation),
+                ),
         )
         println("${outFile.absolutePath} took ${System.currentTimeMillis() - time}ms")
     }
