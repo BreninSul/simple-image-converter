9630a12f5c06413eb585d30539108cf6afc95406
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-12-24 13:59:44 +0100

    Ensure `BufferedInputStream` usage only when necessary.
    Updated `ImageIOReader` to check if the input stream is already a `BufferedInputStream` before wrapping, optimizing memory usage and avoiding redundant wrapping.

--- src/main/kotlin/io/github/breninsul/simpleimageconvertor/service/reader/ImageIOReader.kt
+++ src/main/kotlin/io/github/breninsul/simpleimageconvertor/service/reader/ImageIOReader.kt
@@ -34,7 +34,7 @@ open class ImageIOReader(private val order: Int = Int.MAX_VALUE) : ImageReader {
     override fun supportedTypes() = supportedImageTypes
 
     override fun readInternal(fileStream: InputStream, settings: List<Settings>, metadata: ImageMetadata?): ImageOrAnimation{
-        val inputStream = BufferedInputStream(fileStream)
+        val inputStream = if (fileStream is BufferedInputStream) fileStream else BufferedInputStream(fileStream)
         val bufferedImage: BufferedImage = inputStream.use {  ImageIO.read(it)}
         val originalImage = ImmutableImage.fromAwt(bufferedImage)
         return ImageOrAnimation(null, originalImage,metadata)
